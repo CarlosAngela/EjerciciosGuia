8180669100140b74e235ff879e8780984332681f
CarlosAngela <50214155+CarlosAngela@users.noreply.github.com>
2019-05-21 18:43:56 -0500

    Add files via upload

---
+++ Pa159-Ej1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Pa159_Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Grupo: Rosa Elena Jimenez Gonzalez
+            //   Angela Patricia Carlos Siatama
+
+            Console.WriteLine("Ingrese el Primer valor A ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Digite el Primer valor B ");
+            double b = double.Parse(Console.ReadLine());
+
+            double sum = a + b;
+            double sum1 = b + a;
+
+            if ((a >= 0) && (b >= 0))
+            {
+                Console.WriteLine(" \n ");
+
+                Console.WriteLine(" Solucion");
+
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                Console.WriteLine(" \n ");
+
+
+                Console.WriteLine(" Numeros negativos no pueden ser Operados ");
+            }
+
+            if (a < b)
+            {
+                Console.WriteLine(" \n ");
+
+
+                Console.WriteLine($" A es menor que B/ Se invertiran los valores ");
+
+
+                Console.WriteLine(sum1);
+
+
+            }
+        }
+    }
+}
---
+++ Pa159-Ej2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Pa159_Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            //Grupo: Rosa Elena Jimenez Gonzalez
+            //   Angela Patricia Carlos Siatama
+
+
+            Console.WriteLine("Ejercio de Sumatoria");
+            Console.WriteLine("\n");
+            Console.WriteLine("b\n Σ 1 / x+ay \n a=0");
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine("Digite el valor para b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el valor para  x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el valor para y: ");
+            double y = double.Parse(Console.ReadLine());
+
+
+            for (int a=0; a < b; a++)
+            {
+                double opera = 1 / x + a * y;
+                Console.WriteLine($"Su resultado es {opera}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
